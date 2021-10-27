e84ce817bb6d95be03bfc73c89634695bd90c435
shailajaBegari <89687827+shailajaBegari@users.noreply.github.com>
2021-10-27 17:09:47 +0530

    Add files via upload

--- exam.js
+++ exam.js
@@ -1,10 +1,112 @@
 
-function ShowMessage(firstName, lastName) {
-    console.log( firstName + " " + lastName);
-}
+// // function ShowMessage(firstName, lastName) {
+// //     console.log( firstName + " " + lastName);
+// // }
 
-ShowMessage("Steve", "Jobs", "Mr.");  
-ShowMessage(); 
+// // ShowMessage("Steve", "Jobs", "Mr.");  
+// // ShowMessage(); 
 
-//if u pass less arguments then rest of the parameters will be undefined. 
-// if you pass more arguments then additional arguments will be ignored.
+// // //if u pass less arguments then rest of the parameters will be undefined. 
+// // // if you pass more arguments then additional arguments will be ignored.
+
+
+// // in these return aslo we can pass the functions
+
+
+// //fierst class functions in javascript********************.////
+
+// // var a=function(n){
+// //     return function b(){
+// //     }
+// // }
+// // console.log(a())
+
+
+
+// /// in this we can pass the argument  in the place function name we can pass the functions////
+
+// // var a=function(pa1){
+// //     console.log(pa1)
+// // }
+// // function b(){
+
+// // }
+// // a(b)
+
+
+// //varaible examples//
+// // console.log(a)
+// // var a=100;
+// // {
+// //     var a =10;
+//     // console.log(a)
+// // }
+// // console.log(a)
+
+// //let exmaples//
+
+// // let a=100;
+// // {
+// //     var  a=10;
+// //     // console.log(a)
+// // }
+// // console.log(a)
+// // //this is output is a has already been delcared because let not reassinging the value //
+
+
+// // let x=function(){
+// //     console.log("shaiu")
+// // }
+// // let y=function(call){
+// //     console.log("nani")
+// //     call()
+// // }
+// // y(x)
+
+// //let concept/////////////////
+// // let a=10;
+// //     a=1;
+// // console.log(a)
+// //here let it will take updation but not taking redlclaraton//
+
+
+// // a()
+// // function a(){
+// //     let b=10
+// // }
+// // console.log(b)
+
+// var tester = "hey hi";    
+// function newFunction() {
+//         var hello = "hello";
+//     }
+// console.log(hello); 
+// // console.log(tester); 
+
+
+// // var a=2;
+// // var a=8;
+// // var  a=9
+// // console.log(a)
+
+// let greeting = "say Hi";
+// let times = 4;
+
+// if (times > 3) {
+//      let hello = "say Hello instead";
+//      console.log(hello);// "say Hello instead"
+//  }
+// // console.log(hello) // hello is not defined
+
+// let greeting = "say Hi";
+// let greeting = "say Hello ";
+// console.log(greeting)
+
+//let concept/////////////
+// let greeting = "say Hi";
+//     if (true) {
+//         let greeting = "say Hello instead";
+//         console.log(greeting);       // "say Hello instead"
+//     }
+// console.log(greeting);           // "say Hi"//
+
